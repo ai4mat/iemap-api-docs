0dafee9643e3bc09ec7e41f0d5f3ccfbcb849c06
mpuccini <puccini.marco@gmail.com>
2022-05-31 18:00:13 +0200

    fix: versione fixed

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ENEA'
 author = 'Sergio Ferlito, Marco Puccini, Claudio Ronchetti'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
